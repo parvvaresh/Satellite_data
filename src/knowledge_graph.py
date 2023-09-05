1c80b9567b0d6e4d7b21a60dc8760c174989e5c8
Alireza Parvaresh <89921883+parvvaresh@users.noreply.github.com>
2023-09-05 11:35:03 +0330

    Update knowledge_graph.py

--- src/knowledge_graph.py
+++ src/knowledge_graph.py
@@ -1,8 +1,12 @@
 from process_data import process_data
 from make_graph import create_graph_all, create_graph_special
 
-def knowledge_graph(data, col, rel , all = False, name1 = "1.png", name2 = "2.png"):
-	df = process_data(data, col = "sentence")
-	if all:
-		create_graph_all(df, name = name1)
-	create_graph_special(df, k = 0.5, rel = rel,  name = name2)
+class knowledge_graph:
+	def __init__(self, data, col):
+		self.df = process_data(data, col = "sentence")
+
+	def get_all_relation(self, name):
+		create_graph_all(self.df, name = name)
+
+	def get_with_relation(self, rel, name):
+		create_graph_special(self.df, k = 0.5, rel = rel,  name = name)
