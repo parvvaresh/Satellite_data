91d328952415fc77785c5a7604e1007733210c91
reza <parvvaresh@gmail.com>
2024-08-04 16:12:24 +0330

    new version

--- process/utils.py
+++ process/utils.py
@@ -304,3 +304,35 @@ def check_column(bands : list,
         if band.upper() in col.upper():
             return True
     return False
+
+
+
+def mean_std(df : pd.DataFrame) -> None:
+    _bands_per_date = bands_per_date(df)
+    _all_bands = get_all_bands(_bands_per_date)
+    _all_dates = get_all_dates(_bands_per_date)
+    print(_all_dates)
+    mean = []
+    std = []
+
+    for date in _all_dates:
+        sample_bands_mean = []
+        sample_bands_std = []
+        for band in _all_bands:
+            name_column = f"{date}_{band}"
+            data = df[name_column]
+            _mean = data.dropna().mean()
+            sample_bands_mean.append(_mean)
+
+
+            _std = data.dropna().std()
+            sample_bands_std.append(_std)
+        mean.append(np.array(sample_bands_mean))
+        std.append(np.array(sample_bands_std))
+    
+
+    mean = np.array(mean)
+    std =  np.array(std)
+    
+    result = (mean, std)
+    return result
