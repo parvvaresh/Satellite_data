2520c820949b80bcc99e7cc02325328b35dc63db
reza <parvvaresh@gmail.com>
2024-08-28 01:10:04 +0330

    add label json : encode label

--- process/data/parser.py
+++ process/data/parser.py
@@ -5,9 +5,20 @@ def get_path_script():
     return os.path.dirname(os.path.abspath(__file__))
 
 
-def get_bannds():
+def get_bannds() -> dict:
     spectrums_file = os.path.join(get_path_script(), "bands.json")
 
     with open(spectrums_file, "r") as json_file:
         spectrums = json.load(json_file)
     return spectrums
+
+
+
+
+def get_classindex() -> dict:
+
+    classindex_file = os.path.join(get_path_script(), "classindex.json")
+
+    with open(classindex_file, "r") as json_file:
+        classindex = json.load(json_file)
+    return classindex
